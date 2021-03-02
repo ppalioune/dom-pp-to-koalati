71e14543b552d7b72fb4eef8f5506c4500ab1d83
ppalioune <ppalioune@gmail.com>
2021-03-02 13:23:08 -0500

    add test file

---
+++ domppToKoalati.js
@@ -0,0 +1,15 @@
+//import {getVerdict} from './node_modules/koalati-dompp/index.mjs'
+
+import {evaluateDom} from '../dom-pp/index.mjs'
+console.log(evaluateDom);
+
+class DomppToKoalati {
+    constructor(){}
+
+    getTree(){
+        console.log(evaluateDom);
+    }
+   
+}
+
+export { DomppToKoalati };
