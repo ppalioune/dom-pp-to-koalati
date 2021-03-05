8cb7d49865d5adda62cd06b1ab6933841ce7e261
ppalioune <ppalioune@gmail.com>
2021-03-05 15:06:54 -0500

    add method addresultkoalati and getResultKoalati

--- domppToKoalati.js
+++ domppToKoalati.js
@@ -6,26 +6,29 @@ class DomppToKoalati {
     constructor(cssProperty, tagName) {
         this.cssProperty = cssProperty;
         this.tagName = tagName
-
         //Build result by result-builder
         this.builder = new ResultBuilder()
     }
 
     addResultKoalati(tree, verdict){
-        //get the value to use as koalati results
+        //get the value to use as koalati results (use tree result)
         const cssProperty = tree._rootNode._data.elementAttribute
         const tagName = tree._rootNode._data.subject
         const valueTest = tree._currentNode._data.subject
-
         //use testCondition comme title (use verdicts)
         const title = verdict.condition.name
 
         //build result using ResultBuilder
+        let score = 1
         const result = this.builder.newTest(tagName)
         result.setTitle(title)
               .setDescription("Test performed on " +tagName + " with " + cssProperty + " and value" + valueTest)
-    
+              .setScore(score)
     }
 
-
+    getResultKoalati(){
+        return this.builder.toArray()
+    }
 }
+
+module.exports = DomppToKoalati;
---
+++ test/test.mjs
@@ -0,0 +1,133 @@
+//var assert = require('assert');
+/**
+ * Imports
+ */
+// Chai for assertions
+import pkg_chai from "chai";
+const { expect } = pkg_chai;
+
+// JSDOM for DOM trees
+import pkg_jsdom from "jsdom";
+const { JSDOM } = pkg_jsdom;
+import "jsdom-global";
+
+//import file domppToKoalati
+import DomppToKoalati from '../domppToKoalati.js'
+console.log(DomppToKoalati);
+
+// DataTree for tree management
+//import pkg_datatree from "data-tree";
+//const { dataTree } = pkg_datatree;
+
+// Local imports
+import { evaluateDom, getTreeFromWitness } from 'koalati-dompp';
+import {
+  ComposedFunction,
+  CompoundDesignator,
+  DimensionWidth,
+  FindBySelector,
+  GreaterThan,
+  TestCondition,
+  TestDriver,
+  TestResult,
+  UniversalQuantifier,
+} from 'koalati-dompp';
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      //assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+describe("Witness tests", () => {
+  it("Simple condition true", async () => {
+    var f = new ComposedFunction(new GreaterThan(), "@0", 50);
+    var cond = new TestCondition("A condition", f);
+    var driver = new TestDriver(cond);
+    driver.evaluateAll(100);
+    var result = driver.getResult();
+    expect(result).to.be.an.instanceof(TestResult);
+    expect(result.getResult()).to.be.true;
+    var verdicts = result.getVerdicts();
+    expect(verdicts.length).to.equal(1);
+    var verdict = verdicts[0];
+    var witness = verdict.getWitness();
+    expect(Array.isArray(witness)).to.be.true;
+    expect(witness.length).to.equal(2);
+    const trees = getTreeFromWitness(witness);
+  });
+  it("Simple condition false", async () => {
+    var f = new ComposedFunction(new GreaterThan(), "@0", 50);
+    var cond = new TestCondition("A condition", f);
+    var driver = new TestDriver(cond);
+    driver.evaluateAll(0);
+    var result = driver.getResult();
+    expect(result).to.be.an.instanceof(TestResult);
+    expect(result.getResult()).to.be.false;
+    var verdicts = result.getVerdicts();
+    expect(verdicts.length).to.equal(1);
+    var verdict = verdicts[0];
+    var witness = verdict.getWitness();
+    expect(Array.isArray(witness)).to.be.true;
+    expect(witness.length).to.equal(2);
+    const trees = getTreeFromWitness(witness);
+  });
+  it("Test", async () => {
+    var dom = await load_dom("../dom-pp/test/pages/stub-1.html");
+    var body = dom.window.document.body;
+    var f = new UniversalQuantifier(
+      "$x",
+      new FindBySelector("#h2"),
+      new ComposedFunction(
+        new GreaterThan(),
+        new ComposedFunction(new DimensionWidth(), "$x"),
+        350
+      )
+    );
+
+    var cond = new TestCondition("h2's width > 350", f);
+    var driver = new TestDriver(cond);
+    driver.evaluateAll(body);
+    var result = driver.getResult();
+    expect(result).to.be.an.instanceof(TestResult);
+    expect(result.getResult()).to.be.false;
+    var verdicts = result.getVerdicts();
+    expect(verdicts.length).to.equal(1);
+    var verdict = verdicts[0];
+    var witness = verdict.getWitness();
+    expect(Array.isArray(witness)).to.be.true;
+    expect(witness.length).to.equal(2);
+    const trees = getTreeFromWitness(witness);
+    console.log(trees);
+    //test for dom to kaolati
+    const cssProperty = trees._rootNode._data.elementAttribute
+    const tagName = trees._rootNode._data.subject
+    const valueTest = trees._currentNode._data.subject
+    //console.log(trees._rootNode._data.elementAttribute);
+    console.log(cssProperty, tagName, valueTest, verdict.condition.name);
+
+    let formatDomKoalati = new DomppToKoalati(cssProperty, tagName);
+    const a = formatDomKoalati.addResultKoalati(trees,verdict)
+    console.log(formatDomKoalati.getResultKoalati());
+    
+  });
+});
+
+
+
+
+
+
+/**
+ * Reads a DOM from a file. This function is only meant to avoid cluttering
+ * the code with promises and anonymous functions in every test case.
+ * @param {String} filename The name of the local file to read from
+ * @param A promise which, when fulfilled, returns the DOM object.
+ */
+async function load_dom(filename) {
+  return JSDOM.fromFile(filename).then((dom) => {
+    return dom;
+  });
+}
